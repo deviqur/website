552fb5dee67cc25d154d251c0c218b801c5fbf09
Devi Qurnia Sari <deviqurnia10@gmail.com>
2019-04-16 16:41:58 +0700

    Updates

---
+++ precache-manifest.cf4c5f999d79073867a903afd022961f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "a25770da661e0f50a86c",
+    "url": "/website/static/js/main.a4288baa.chunk.js"
+  },
+  {
+    "revision": "a409fb8ccd8ddd7c3013",
+    "url": "/website/static/js/2.ef4f2b19.chunk.js"
+  },
+  {
+    "revision": "a25770da661e0f50a86c",
+    "url": "/website/static/css/main.ad20621e.chunk.css"
+  },
+  {
+    "revision": "1e0c59cf7065310a760d207a1eba99d0",
+    "url": "/website/index.html"
+  }
+];
